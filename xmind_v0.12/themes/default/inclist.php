ad2d5437572a95618db9b0631dea8d1393641ed2
jicky <jicky>
2005-09-21 17:46:24 +0000

    *** empty log message ***

--- xmind_v0.12/themes/default/inclist.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-<link href="style.css" type="text/css" rel="stylesheet">
-</head>
-<body leftmargin="0" topmargin="0" class="inclist" onload="parent.ClistDefault<?php echo $clist; ?>()">
-<div id="content">
-</div>
-</body>
-</html>
