526f0898c4f5b9d12a6a976c52db53a27cd3e78d
favnec <favnec>
2005-09-24 12:54:48 +0000

    *** empty log message ***

--- xmind_v0.1.2/Xmind.php
+++ xmind_v0.1.2/Xmind.php
@@ -6,7 +6,7 @@
 
  Xmind project (parse core)
  Auteur : Thomas Favennec
- version  0.1.2d alpha
+ version  0.1.2e alpha
  support www.redsofa.net
 
  This file is part of Xmind project.
@@ -85,10 +85,9 @@
    $string=str_replace("\n", '', $string);
    
    $xml_parser = xml_parser_create();
-   xml_set_character_data_handler($xml_parser, "XMLparseHandler");
-   //xml_set_processing_instruction_handler($xml_parser, "XMLparseNdata");
-   xml_set_element_handler($xml_parser, "XMLparseDebutElement", "XMLparseFinElement");
-   if (!xml_parse($xml_parser, $string)) die(sprintf("Xmind XML error : %s à la ligne %d", xml_error_string(xml_get_error_code($xml_parser)), xml_get_current_line_number($xml_parser) )) ;
+   xml_set_character_data_handler($xml_parser,'XMLparseHandler');
+   xml_set_element_handler($xml_parser,'XMLparseDebutElement','XMLparseFinElement');
+   if (!xml_parse($xml_parser, $string)) die('Xmind error : Des erreurs XML sont signalée par le parser !') ;
    xml_parser_free($xml_parser);
 
 
@@ -99,12 +98,13 @@
 // utilisé par Xmindparse
 function XMLParseDebutElement($parser, $name, $attrs)
 {
- global $Xstring, $Xpath, $Xoptions, $Xtheme, $Xlasttag, $XlastName, $Xn,
- $Xtemp, $lastNB, $Xintegration, $Yb;
+ global $Xstring,$Xpath,$Xoptions,$Xtheme,$Xlasttag,$XlastName,
+ $Xn,$Yn,$Xbox,$Xtemp,$lastNB,$Xintegration;
 
  $Xoptions=array();
  if(sizeof($attrs)) while (list($k, $v) = each($attrs)) $Xoptions[$k]=$v;
  $Xlasttag=$name;
+ $Yn[$name]++;
 
  if($name=='XMIND')
  {
@@ -120,10 +120,16 @@ function XMLParseDebutElement($parser, $name, $attrs)
  }
  if($name=='SCRIPT') $Xstring.='<script language="javascript">';
  if($name=='CODE') $Xtemp[CODE]=$Xoptions[NAME];
+ if($name=='HBOX'||$name=='VBOX')
+ {
+  if(!$Xoptions[SPAN]) $Xoptions[SPAN]=1;
+  $Xstring.='<table cellpadding="'.$Xoptions[SPAN].'" cellspacing="'.$Xoptions[SPAN].'" height="'.$Xoptions[HEIGHT].'" width="'.$Xoptions[WIDTH].'" align="'.$Xoptions[ALIGN].'" valign="'.$Xoptions[VALIGN].'"><tr><td align="center" valign="middle">';
+  $Xtemp[BOX][++$Xbox]=$name;
+ }
  if($name=='TBOX')
  {
-   if(!$Xoptions[SPAN]) $Xoptions[SPAN]=1;
-   $Xstring.='<table cellpadding="'.$Xoptions[SPAN].'" cellspacing="'.$Xoptions[SPAN].'" height="'.$Xoptions[HEIGHT].'" width="'.$Xoptions[WIDTH].'" align="'.$Xoptions[ALIGN].'" valign="'.$Xoptions[VALIGN].'">';
+  if(!$Xoptions[SPAN]) $Xoptions[SPAN]=1;
+  $Xstring.='<table cellpadding="'.$Xoptions[SPAN].'" cellspacing="'.$Xoptions[SPAN].'" height="'.$Xoptions[HEIGHT].'" width="'.$Xoptions[WIDTH].'" align="'.$Xoptions[ALIGN].'" valign="'.$Xoptions[VALIGN].'">';
  }
  if($name=='TR')$Xstring.='<tr>';
  if($name=='TD')
@@ -142,7 +148,7 @@ function XMLParseDebutElement($parser, $name, $attrs)
  if($name=='NOTEBOOK')
  {
    $Xn[NOTEBOOK]++;
-   $Yb++;
+   $Yb=$Yn[NOTEBOOK];
    if(!$Xoptions[NAME]) $Xoptions[NAME]='notebook'.$Xn[NOTEBOOK];
    $Xtemp[NOTEBOOK][$Yb][1]=$Xoptions[NAME];
    $Xn[PAGE][$Yb]=1;
@@ -153,14 +159,15 @@ function XMLParseDebutElement($parser, $name, $attrs)
    $Xstring.='<table cellpadding="0" cellspacing="0"><tr>';
    for($i=0; $i<count($tab); $i++)
    {
-     $j=$i+1;
-     if($Xtemp[NOTEBOOK][$Yb][0]==$j) $on='On'; else $on='Off';
-     $Xstring.='<td id="Xpage_'.$Xoptions[NAME].'_'.$j.'a" class="pagea'.$on.'" onclick="NoteBookOnclick(\''.$Xoptions[NAME].'\', '.$j.')"></td><td id="Xpage_'.$Xoptions[NAME].'_'.$j.'ab" class="pageab'.$on.'" align="center" valign="middle" onclick="NoteBookOnclick(\''.$Xoptions[NAME].'\', '.$j.')">&nbsp;&nbsp;'.trim($tab[$i]).'&nbsp;&nbsp;</td><td id="Xpage_'.$Xoptions[NAME].'_'.$j.'b" class="pageb'.$on.'" onclick="NoteBookOnclick(\''.$Xoptions[NAME].'\', '.$j.')"></td>';
+    $j=$i+1;
+    if($Xtemp[NOTEBOOK][$Yb][0]==$j) $on='On'; else $on='Off';
+    $Xstring.='<td id="Xpage_'.$Xoptions[NAME].'_'.$j.'a" class="pagea'.$on.'" onclick="NoteBookOnclick(\''.$Xoptions[NAME].'\', '.$j.')"></td><td id="Xpage_'.$Xoptions[NAME].'_'.$j.'ab" class="pageab'.$on.'" align="center" valign="middle" onclick="NoteBookOnclick(\''.$Xoptions[NAME].'\', '.$j.')">&nbsp;&nbsp;'.trim($tab[$i]).'&nbsp;&nbsp;</td><td id="Xpage_'.$Xoptions[NAME].'_'.$j.'b" class="pageb'.$on.'" onclick="NoteBookOnclick(\''.$Xoptions[NAME].'\', '.$j.')"></td>';
    }
    $Xstring.='</tr></table></td><td class="pageb"></td></tr><tr><td class="pageac"></td><td>';
  }
  if($name=='PAGE')
  {
+   $Yb=$Yn[NOTEBOOK];
    $Xstring.='<div id="Xpage_'.$Xtemp[NOTEBOOK][$Yb][1].'_'.$Xn[PAGE][$Yb].'" style="width:100%; height:100%; display:';
    if($Xtemp[NOTEBOOK][$Yb][0]==$Xn[PAGE][$Yb]) $Xstring.='true'; else $Xstring.='none';
    $Xstring.=';"><table cellpadding="'.$Xoptions[MARGE].'" cellspacing="'.$Xoptions[MARGE].'" height="100%" width="100%"><tr><td height="100%" width="100%" align="'.$Xoptions[ALIGN].'" valign="'.$Xoptions[VALIGN].'">';
@@ -181,8 +188,8 @@ function XMLParseDebutElement($parser, $name, $attrs)
   $Xtemp[0]=explode(',', trim($Xoptions[CALIGN]));
   for($i=0; $i<$Xoptions[COLUMNS]; $i++)
   {
-     if($w[$i]<50) $w[$i]=50;
-     if(!$w[$i]||$w[$i]<strlen($t[$i])*8+14) $w[$i]=strlen($t[$i])*8+15;
+   if($w[$i]<50) $w[$i]=50;
+   if(!$w[$i]||$w[$i]<strlen($t[$i])*8+14) $w[$i]=strlen($t[$i])*8+15;
   }
   $Xtemp[1]=$w;
   if($Xoptions[WIDTH]<array_sum($w)+12) $Xoptions[WIDTH]=array_sum($w)+24;
@@ -203,7 +210,6 @@ function XMLParseDebutElement($parser, $name, $attrs)
 
 
 
-
 // utilisé par Xmindparse
 function XMLparseHandler($parser, $data)
 {
@@ -217,95 +223,97 @@ function XMLparseHandler($parser, $data)
 // utilisé par Xmindparse
 function XMLparseFinElement($parser, $name)
 {
- global $Xstring, $Xpath, $Xoptions, $Xtheme, $Xlasthandler, $defaultVars, $Xlasttag, $XlastName,
- $Xtemp, $Xn, $Xintegration, $Yb, $Xcode;
+ global $Xstring, $Xpath, $Xoptions, $Xtheme, $Xlasthandler, $defaultVars,
+ $Xlasttag,$XlastName,$Xtemp,$Xn,$Yn,$Xbox,$Xintegration,$Xcode;
 
  if($name=='SCRIPT') $Xstring.=$Xlasthandler.'</script>';
  if($name=='CODE') if ($Xtemp[CODE])
  {
-   session_start();
-   session_register('Xcode');
-   $Xcode[$Xtemp[CODE]]=$Xlasthandler;
+  session_start();
+  session_register('Xcode');
+  $Xcode[$Xtemp[CODE]]=$Xlasthandler;
  }
- if($name=='TBOX')$Xstring.='</table>';
+ if($name=='HBOX'||$name=='VBOX')
+ {
+  $Xtemp[BOX][$Xbox--]='';
+  $Xstring.='</td></tr></table>'.XmindBetweenBox($Xtemp,$Xbox);
+ }
+ if($name=='TBOX') $Xstring.='</table>'.XmindBetweenBox($Xtemp,$Xbox);
  if($name=='TR')$Xstring.='</tr>';
  if($name=='TD')$Xstring.='</td>';
  if($name=='LAYER') $Xstring.='</div>';
- if($name=='FRAME') $Xstring.='</td></tr></table></td><td class="framebd"></td></tr><tr><td class="framec"></td><td class="framecd"></td><td class="framed"></td></tr></table>';
- if($name=='NOTEBOOK')
- {
-  $Xstring.='</td><td class="pagebd"></td></tr><tr><td class="pagec"></td><td class="pagecd"></td><td class="paged"></td></tr></table>';
-  $Yb--;
- }
+ if($name=='FRAME') $Xstring.='</td></tr></table></td><td class="framebd"></td></tr><tr><td class="framec"></td><td class="framecd"></td><td class="framed"></td></tr></table>'.XmindBetweenBox($Xtemp,$Xbox);
+ if($name=='NOTEBOOK') $Xstring.='</td><td class="pagebd"></td></tr><tr><td class="pagec"></td><td class="pagecd"></td><td class="paged"></td></tr></table>'.XmindBetweenBox($Xtemp,$Xbox);
  if($name=='PAGE') $Xstring.='</td></tr></table></div>';
- if($name=='LABEL') $Xstring.=XmindLabel($Xlasthandler,$Xoptions[NAME],$Xoptions[STYLE]);
+ if($name=='LABEL') $Xstring.=XmindLabel($Xlasthandler,$Xoptions[NAME],$Xoptions[STYLE]).XmindBetweenBox($Xtemp,$Xbox);
  if($name=='ENTRY')
  {
   $Xoptions[ONCHANGE]=str_replace("'", "\\'", $Xoptions[ONCHANGE]);
   $Xoptions[ONCHANGE]=str_replace("'", "\\'", $Xoptions[ONBLUR]);
-  $Xstring.=XmindEntry($Xtheme,$Xoptions[NAME],$Xoptions[TYPE],$Xoptions[WIDTH],$Xlasthandler,$Xoptions[ACTIVE],$Xoptions[ONCHANGE],$Xoptions[ONBLUR]);
+  $Xstring.=XmindEntry($Xtheme,$Xoptions[NAME],$Xoptions[TYPE],$Xoptions[WIDTH],$Xlasthandler,$Xoptions[ACTIVE],$Xoptions[ONCHANGE],$Xoptions[ONBLUR]).XmindBetweenBox($Xtemp,$Xbox);
  }
  if($name=='TEXT')
  {
   $Xoptions[ONCHANGE]=str_replace("'", "\\'", $Xoptions[ONCHANGE]);
   $Xoptions[ONCHANGE]=str_replace("'", "\\'", $Xoptions[ONBLUR]);
-  $Xstring.=XmindText($Xtheme,$Xoptions[NAME], $Xoptions[WIDTH],$Xoptions[HEIGHT],$Xlasthandler,$Xoptions[ACTIVE],$Xoptions[ONCHANGE],$Xoptions[ONBLUR]);
+  $Xstring.=XmindText($Xtheme,$Xoptions[NAME], $Xoptions[WIDTH],$Xoptions[HEIGHT],$Xlasthandler,$Xoptions[ACTIVE],$Xoptions[ONCHANGE],$Xoptions[ONBLUR]).XmindBetweenBox($Xtemp,$Xbox);
  }
  if($name=='HIDDEN') $Xstring.=XmindHidden($Xoptions[NAME],$Xlasthandler);
  if($name=='SPIN')
  {
   $Xoptions[ONCHANGE]=str_replace("'", "\\'", $Xoptions[ONCHANGE]);
   $Xoptions[ONCHANGE]=str_replace("'", "\\'", $Xoptions[ONBLUR]);
-  $Xstring.=XmindSpin($Xtheme,$Xoptions[NAME],$Xoptions[WIDTH],$Xlasthandler,$Xoptions['MIN'],$Xoptions['MAX'],$Xoptions[STEP],$Xoptions[ONCHANGE],$Xoptions[ONBLUR],$Xoptions[ACTIVE]);
+  $Xstring.=XmindSpin($Xtheme,$Xoptions[NAME],$Xoptions[WIDTH],$Xlasthandler,$Xoptions['MIN'],$Xoptions['MAX'],$Xoptions[STEP],$Xoptions[ONCHANGE],$Xoptions[ONBLUR],$Xoptions[ACTIVE]).XmindBetweenBox($Xtemp,$Xbox);
  }
  if($name=='BUTTON')
  {
   $Xoptions[ONCLICK]=str_replace("'", "\\'", $Xoptions[ONCLICK]);
-  $Xstring.=XmindButton($Xtheme,$Xoptions[NAME],$Xlasthandler,$Xoptions[IMAGE],$Xoptions[ONCLICK],$Xoptions[VALUE],$Xoptions[ACTIVE], $Xoptions[WIDTH]);
+  $Xstring.=XmindButton($Xtheme,$Xoptions[NAME],$Xlasthandler,$Xoptions[IMAGE],$Xoptions[ONCLICK],$Xoptions[VALUE],$Xoptions[ACTIVE], $Xoptions[WIDTH]).XmindBetweenBox($Xtemp,$Xbox);
  }
  if($name=='TOGGLE')
  {
   $Xoptions[ONCLICK]=str_replace("'", "\\'", $Xoptions[ONCLICK]);
-  $Xstring.=XmindToggle($Xtheme,$Xoptions[NAME],$Xlasthandler,$Xoptions[VALUE],$Xoptions[ONCLICK],$Xoptions[ACTIVE]);
+  $Xstring.=XmindToggle($Xtheme,$Xoptions[NAME],$Xlasthandler,$Xoptions[VALUE],$Xoptions[ONCLICK],$Xoptions[ACTIVE]).XmindBetweenBox($Xtemp,$Xbox);
  }
  if($name=='CHECKBOX')
  {
   $Xoptions[ONCLICK]=str_replace("'", "\\'", $Xoptions[ONCLICK]);
-  $Xstring.=XmindCheckBox($Xtheme,$Xoptions[NAME],$Xoptions[VALUE],$Xoptions[ONCLICK],$Xoptions[ACTIVE]);
+  $Xstring.=XmindCheckBox($Xtheme,$Xoptions[NAME],$Xoptions[VALUE],$Xoptions[ONCLICK],$Xoptions[ACTIVE]).XmindBetweenBox($Xtemp,$Xbox);
  }
  if($name=='RADIO')
  {
   $Xoptions[ONCLICK]=str_replace("'", "\\'",$Xoptions[ONCLICK]);
-  $Xstring.=XmindRadio($Xtheme,$Xoptions[NAME],$Xlasthandler,$Xoptions[VALUE],$Xoptions[ONCLICK],$Xoptions[ACTIVE]);
+  $Xstring.=XmindRadio($Xtheme,$Xoptions[NAME],$Xlasthandler,$Xoptions[VALUE],$Xoptions[ONCLICK],$Xoptions[ACTIVE]).XmindBetweenBox($Xtemp,$Xbox);
  }
- if($name=='PROGRESSBAR')$Xstring.=XmindProgressBar($Xtheme,$Xoptions[NAME],$Xoptions[WIDTH],$Xoptions[VALUE]);
- if($name=='IMAGE')$Xstring.=XmindImage($Xoptions[NAME],$Xoptions[SRC],$Xoptions[WIDTH],$Xoptions[HEIGHT]);
- if($name=='DRAWINGAREA')$Xstring.=XmindDrawingArea($Xtheme,$Xoptions[NAME],$Xoptions[WIDTH],$Xoptions[HEIGHT],$Xoptions[SRC]);
+ if($name=='PROGRESSBAR')$Xstring.=XmindProgressBar($Xtheme,$Xoptions[NAME],$Xoptions[WIDTH],$Xoptions[VALUE]).XmindBetweenBox($Xtemp,$Xbox);
+ if($name=='IMAGE')$Xstring.=XmindImage($Xoptions[NAME],$Xoptions[SRC],$Xoptions[WIDTH],$Xoptions[HEIGHT]).XmindBetweenBox($Xtemp,$Xbox);
+ if($name=='DRAWINGAREA') $Xstring.=XmindDrawingArea($Xtheme,$Xoptions[NAME],$Xoptions[WIDTH],$Xoptions[HEIGHT],$Xoptions[SRC]).XmindBetweenBox($Xtemp,$Xbox);
  if($name=='CLIST')
  {
   $Xlasttag='';
   $Xstring.='</tbody></table>\'; }</script>';
+  $Xstring.=XmindBetweenBox($Xtemp,$Xbox);
  }
  if($name=='OPTION')
  {
-   if($Xlasttag='CLIST')
+  if($Xlasttag='CLIST')
+  {
+   $Xstring.='<tr onclick="parent.ClistOnClick(this, \\\''.$XlastName.'\\\', \\\''.trim($Xlasthandler).'\\\')">';
+   $t=explode(',',trim($Xlasthandler));
+   $i=0;
+   foreach($t as $t2)
    {
-    $Xstring.='<tr onclick="parent.ClistOnClick(this, \\\''.$XlastName.'\\\', \\\''.trim($Xlasthandler).'\\\')">';
-    $t=explode(',',trim($Xlasthandler));
-    $i=0;
-    foreach($t as $t2)
+    $n=round($Xtemp[1][$i]/8-2);
+    if(strlen($t2)>$n)
     {
-     $n=round($Xtemp[1][$i]/8-2);
-     if(strlen($t2)>$n)
-     {
-      $t2{$n}=',';
-      list($t2,)=explode(',',$t2);
-     }
-     $Xstring.='<td></td><td  align="'.trim($Xtemp[0][$i]).'">&nbsp;'.trim($t2).'</td><td></td>';
-     $i++;
+     $t2{$n}=',';
+     list($t2,)=explode(',',$t2);
     }
-    $Xstring.='</tr>';
+    $Xstring.='<td></td><td  align="'.trim($Xtemp[0][$i]).'">&nbsp;'.trim($t2).'</td><td></td>';
+    $i++;
    }
+   $Xstring.='</tr>';
+  }
 
  }
 
@@ -321,10 +329,20 @@ function XMLparseFinElement($parser, $name)
   if($Xintegration!='plain') $Xstring.='</body></html>';
  }
 
- $Xlasttag='';
+
+ $Yn[$name]--;
+ 
+ 
 }
 
 
+// spéciphiques aux Hbox et Vbox
+function XmindBetweenBox($Xtemp,$Xbox)
+{
+  if($Xtemp[BOX][$Xbox]=='HBOX') return('</td><td align="center" valign="middle">');
+  else if($Xtemp[BOX][$Xbox]=='VBOX') return('</td></tr><tr><td align="center" valign="middle">');
+  else return('');
+}
 
 
 
